f787dc0dc8576c3840ff077c918d41b9d0ec0414
Sila Setthakan-anan <styxmaz@gmail.com>
2026-07-02 02:10:16 +0700

    Update reverseString function implementation
    Refactor reverseString implementation using foldr.

--- haskell/003_reverse-string/src/ReverseString.hs
+++ haskell/003_reverse-string/src/ReverseString.hs
@@ -1,7 +1,11 @@
 module ReverseString (reverseString) where
 
-reverseString :: String -> String
+-- reverseString :: String -> String
 -- reverseString = reverse
+
+-- reverseString :: String -> String
 -- reverseString ""     = ""
 -- reverseString (x:xs) = reverseString xs ++ [x]
+
+reverseString :: String -> String
 reverseString = foldr (\x xs -> xs ++ [x]) []
