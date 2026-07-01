e2e9b97ab611294bcd35045a5ba6c56cea621e2c
fResult <styxmaz@gmail.com>
2026-07-02 00:26:37 +0700

    Solve darts using Euclidean distance guard clauses

--- haskell/004_darts/src/Darts.hs
+++ haskell/004_darts/src/Darts.hs
@@ -1,4 +1,10 @@
 module Darts (score) where
 
 score :: Float -> Float -> Int
-score x y = error "You need to implement this function."
+score x y
+  | distanceFromOrigin > 10 = 0
+  | distanceFromOrigin > 5  = 1
+  | distanceFromOrigin > 1  = 5
+  | otherwise               = 10
+  where
+    distanceFromOrigin = sqrt ((x ** 2) + (y ** 2))
