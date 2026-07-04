889dfaf6126b17fe73b487f33d62d4bce2f4696f
fResult <styxmaz@gmail.com>
2026-07-04 13:28:24 +0700

    Optimize `reverseString` from O(n²) `foldr` to O(n) `foldl'` with cons prepend

--- haskell/003_reverse-string/src/ReverseString.hs
+++ haskell/003_reverse-string/src/ReverseString.hs
@@ -1,11 +1,22 @@
 module ReverseString (reverseString) where
 
+import Data.List (foldl')
+
 -- reverseString :: String -> String
 -- reverseString = reverse
 
+-- Time: O(n²), Space: O(n²)
 -- reverseString :: String -> String
 -- reverseString ""     = ""
 -- reverseString (x:xs) = reverseString xs ++ [x]
 
+-- Time: O(n²), Space: O(n²)
+-- reverseString :: String -> String
+-- reverseString = foldr (\x xs -> xs ++ [x]) ""
+
+-- Time: O(n), Space: O(n)
 reverseString :: String -> String
-reverseString = foldr (\x xs -> xs ++ [x]) []
+reverseString = foldl' (flip prependChar) ""
+
+prependChar :: Char -> String -> String
+prependChar = (:)
