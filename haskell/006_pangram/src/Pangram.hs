07236c72169bab35857b229c85c49eb746e3a9ca
fResult <styxmaz@gmail.com>
2026-07-06 00:25:40 +0700

    Solve `pangram` using `Data.Set` membership and `nub` filtering

--- haskell/006_pangram/src/Pangram.hs
+++ haskell/006_pangram/src/Pangram.hs
@@ -1,4 +1,12 @@
 module Pangram (isPangram) where
 
+import Data.Char (toLower)
+import Data.List (nub)
+import qualified Data.Set as Set
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text = length (filter inAlphs uniqChars) >= Set.size alphs
+  where
+    uniqChars  = (nub . map toLower) text
+    alphs      = Set.fromList ['a' .. 'z']
+    inAlphs ch = ch `Set.member` alphs
