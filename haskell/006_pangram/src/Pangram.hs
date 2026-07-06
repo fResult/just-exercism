70b1101305d0a501ba5788cd37ad7860341f248d
fResult <styxmaz@gmail.com>
2026-07-07 00:18:43 +0700

    Refactor `isPangram` to use `Set.fromList` on filtered input instead of `nub`

--- haskell/006_pangram/src/Pangram.hs
+++ haskell/006_pangram/src/Pangram.hs
@@ -1,12 +1,19 @@
 module Pangram (isPangram) where
 
-import Data.Char (toLower)
-import Data.List (nub)
+import Data.Char (isAsciiLower, toLower)
+-- import Data.List (nub)
 import qualified Data.Set as Set
 
+-- isPangram :: String -> Bool
+-- isPangram text = length (filter inAlphs uniqChars) >= Set.size alphs
+--   where
+--     uniqChars  = (nub . map toLower) text
+--     alphs      = Set.fromList ['a' .. 'z']
+--     inAlphs ch = ch `Set.member` alphs
+
 isPangram :: String -> Bool
-isPangram text = length (filter inAlphs uniqChars) >= Set.size alphs
+isPangram text = Set.size (Set.fromList filteredText) == length alphs
   where
-    uniqChars  = (nub . map toLower) text
-    alphs      = Set.fromList ['a' .. 'z']
-    inAlphs ch = ch `Set.member` alphs
+    filteredText = filter isAsciiLower lowerText
+    lowerText    = map toLower text
+    alphs        = Set.fromList ['a' .. 'z']
