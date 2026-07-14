a3372c4f35eef20b0bca8a5c16a6540efbb1626e
fResult <styxmaz@gmail.com>
2026-07-14 22:43:38 +0700

    Solve `collatz-conjecture` using recursive step counting with `odd`/`even` guards

--- haskell/008_collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/008_collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,16 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = error "You need to implement this function."
+collatz n
+  | n <= 0    = Nothing
+  | otherwise = Just (steps n)
+
+steps :: Integer -> Integer
+steps 1 = 0
+steps n = steps (next n) + 1
+
+next :: Integer -> Integer
+next n
+  | odd n  = n * 3 + 1
+  | even n = n `div` 2
+
