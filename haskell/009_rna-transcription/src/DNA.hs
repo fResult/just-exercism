fcd0b3fe5f3db72453dc7fad38435cae0ac13567
fResult <styxmaz@gmail.com>
2026-07-18 00:38:30 +0700

    Extract `complement` helper in `rna-transcription` for cleaner nucleotide mapping

--- haskell/009_rna-transcription/src/DNA.hs
+++ haskell/009_rna-transcription/src/DNA.hs
@@ -2,9 +2,23 @@ module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
 toRNA ""     = Right ""
-toRNA (x:xs) = case x of
-  'G'                -> ('C' :) <$> toRNA xs
-  'C'                -> ('G' :) <$> toRNA xs
-  'T'                -> ('A' :) <$> toRNA xs
-  'A'                -> ('U' :) <$> toRNA xs
-  invalidNucleotides -> Left invalidNucleotides
+toRNA (x:xs) = case complement x of
+  Just nucleotide -> (nucleotide :) <$> toRNA xs
+  Nothing         -> Left x
+
+complement :: Char -> Maybe Char
+complement 'G' = Just 'C'
+complement 'C' = Just 'G'
+complement 'T' = Just 'A'
+complement 'A' = Just 'U'
+complement _   = Nothing
+
+-- First try
+-- toRNA :: String -> Either Char String
+-- toRNA ""     = Right ""
+-- toRNA (x:xs) = case x of
+  -- 'G'                -> ('C' :) <$> toRNA xs
+  -- 'C'                -> ('G' :) <$> toRNA xs
+  -- 'T'                -> ('A' :) <$> toRNA xs
+  -- 'A'                -> ('U' :) <$> toRNA xs
+  -- invalidNucleotides -> Left invalidNucleotides
